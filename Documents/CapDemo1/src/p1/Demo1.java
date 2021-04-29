aa5efa15cbff46607ddeeb36f5e28eba68dafc04
Lata Verma <lata.soni@gmail.com>
2021-04-29 18:15:12 +0530

    v2

--- Documents/CapDemo1/src/p1/Demo1.java
+++ Documents/CapDemo1/src/p1/Demo1.java
@@ -3,7 +3,7 @@ package p1;
 public class Demo1 {
 
 	public static void main(String[] args) {
-		int a=Integer.parseInt(args[0])+Integer.parseInt(args[1]);
+		int a=Integer.parseInt(args[1])+Integer.parseInt(args[2]);
 		System.out.println("Pi Value is: "+Math.PI);
 		System.out.println("a is "+a);
 	}
