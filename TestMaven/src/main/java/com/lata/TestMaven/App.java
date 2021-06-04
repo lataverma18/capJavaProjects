d87606a6decdc1d1d6cdd7d3b29746883c6f9fe1
lataverma18 <79335527+lataverma18@users.noreply.github.com>
2021-06-05 02:10:52 +0530

    Add files via upload

---
+++ TestMaven/src/main/java/com/lata/TestMaven/App.java
@@ -0,0 +1,18 @@
+package com.lata.TestMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	static int sum(int a,int b)
+	{
+		return a+b;
+	}
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println(sum(10,20));
+    }
+}
